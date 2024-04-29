6ca1e60d764444cced47bb58f89efb20c5cd42fe
Sameer Singh <136222372+sameersinghharry2000@users.noreply.github.com>
2024-04-29 16:17:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById("product_menu_hover").addEventListener("click",function(){
     document.querySelector(".products_menu").style.display="block";
 })
+
+
+let navbtn = document.getElementById("navmenuline");
+let slider = document.querySelector(".navslider");
+
+navbtn.addEventListener("click",()=>{
+    if(navbtn.style.display="block"){
+        slider.classList.add("navsliderzero");
+    }
+})
+
+
+
+
+
